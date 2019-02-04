5b899c757babdfe461f674404242b0900231e23b
anujanagarkar <anujan1134@gmail.com>
2019-02-04 17:39:29 +0530

    - Added Directive for Password & ConfirmPassword match.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { APP_IMPORTS } from './app.imports';
-
+import { ConfirmEqualValidatorDirective } from './shared/validators/confirm-equal-validator.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthInterceptor } from './shared/auth/auth.interceptors';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ConfirmEqualValidatorDirective
   ],
   imports: [
     BrowserModule,
