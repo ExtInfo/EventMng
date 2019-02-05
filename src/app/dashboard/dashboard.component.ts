e2a13c87e2993d0e0fed3bf6f0c1d501899f8194
tushar123phalke <tushar123phalke@gmail.com>
2019-02-05 11:25:02 +0530

    - Added Dashboard Component.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  isHidden: boolean;
+
+  constructor() { 
+    this.isHidden = true;
+  }
 
   ngOnInit() {
   }
 
+  menuToggleHandler(){
+    this.isHidden = !this.isHidden
+  }
+  getClasses (value:boolean) {
+    let newClasses;
+    if (value) {
+      newClasses = {
+        'sidenavOpen' : true,
+        'sidenavClose' : false,
+      } 
+    } else {
+      newClasses = {
+        'sidenavOpen' : false,
+        'sidenavClose' : true,
+      } 
+    }
+    return newClasses;
+  }
+
+  getClass (value:boolean) {
+    let getClass;
+    if (value) {
+      getClass = {
+        'menutoggleOpen' : true,
+        'menutoggleClose' : false,
+      } 
+    } else {
+      getClass = {
+        'menutoggleOpen' : false,
+        'menutoggleClose' : true,
+      } 
+    }
+    return getClass;
+  }
 }
