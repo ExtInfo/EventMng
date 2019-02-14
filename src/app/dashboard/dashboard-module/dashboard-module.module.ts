d81c2efae871e699d2c7d5066efc52953e5d526b
tushar123phalke <tushar123phalke@gmail.com>
2019-02-14 11:27:16 +0530

    -- Added Calender component

--- src/app/dashboard/dashboard-module/dashboard-module.module.ts
+++ src/app/dashboard/dashboard-module/dashboard-module.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { jqxCalendarComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxcalendar';
 
 import { DashboardModuleRoutingModule , DashboardRoutingComponenets} from './dashboard-module-routing.module';
 
 @NgModule({
   declarations: [
-    DashboardRoutingComponenets
+    DashboardRoutingComponenets,
+    jqxCalendarComponent
   ],
   imports: [
     CommonModule,
--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
 import { BaseService } from '../home/base.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { jqxCalendarComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxcalendar';
+import { Element } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-home',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   providers: [NgbCarouselConfig]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('eventCalender') eventCalender: jqxCalendarComponent;
+  @ViewChild('calenderPanel') calenderPanel: ElementRef;
   draftCount;
   testArray = [1, 2, 3];
   showNavigationArrows = false;
@@ -22,8 +27,22 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.getAllEventsData();
   }
+  myCalendarEvent (event: any)  {
+    const date = event.args.date;
+    const view = event.args.view;
+    const viewFrom = view.from;
+    const viewTo = view.to;
+    console.log(date, view, viewFrom, viewTo);
+    this.calenderPanel.nativeElement.innerHTML = date.toLocaleDateString();
+  }
+  ngAfterViewInit (){
+    this.eventCalender.addSpecialDate(new Date('02/12/2019'), '' , 'Date 1');
+    this.eventCalender.addSpecialDate(new Date('02/13/2019'), '' , 'Date 2');
+    this.eventCalender.addSpecialDate(new Date('02/14/2019'), '' , 'Date 3');
+  }
 
   getAllEventsData(): void {
+    if(localStorage.userData){
     console.log('userData is here' + JSON.stringify(JSON.parse(localStorage.userData)));
     const uData = JSON.parse(localStorage.userData);
     this.baseService.loadDashboard(uData.userId, uData.userToken).subscribe((eventRes: any) => {
@@ -34,5 +53,7 @@ export class HomeComponent implements OnInit {
     }
     );
   }
+  }
+
 
 }
