144687d30c02b6f93ffef11833dfc0454b56abda
tushar123phalke <tushar123phalke@gmail.com>
2019-02-15 12:16:24 +0530

    - Added Calender, Handled Calender functionality, Handled Sorting of event

--- src/app/dashboard/home/base.service.ts
+++ src/app/dashboard/home/base.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AppConstant } from '../../shared/utility/constant';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
+import { share } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,7 @@ export class BaseService {
     const reqHeaders = {
         'access-token': token
     };
-    return this.http.get(this.rootURL + '/events/getAllEvents', { headers: reqHeaders, params: {uid: userId} });
+    return this.http.get(this.rootURL + '/events/getAllEvents', { headers: reqHeaders, params: {uid: userId} }).pipe(share());
   }
 
 
--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -4,6 +4,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { jqxCalendarComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxcalendar';
 import { Element } from '@angular/compiler/src/render3/r3_ast';
+import { Observable, from, pipe, of} from 'rxjs';
+import { filter} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -15,8 +18,12 @@ import { Element } from '@angular/compiler/src/render3/r3_ast';
 export class HomeComponent implements OnInit, AfterViewInit {
   @ViewChild('eventCalender') eventCalender: jqxCalendarComponent;
   @ViewChild('calenderPanel') calenderPanel: ElementRef;
-  draftCount;
+  draftCount: Number = 0;
+  pendingCount: Number = 0;
+  eventData: any;
+  selectedDateEvents:any;
   testArray = [1, 2, 3];
+  getDateArray: any;
   showNavigationArrows = false;
   showNavigationIndicators = false;
   constructor(public baseService: BaseService, config: NgbCarouselConfig) {
@@ -25,35 +32,58 @@ export class HomeComponent implements OnInit, AfterViewInit {
    }
 
   ngOnInit() {
-    this.getAllEventsData();
+
   }
   myCalendarEvent (event: any)  {
     const date = event.args.date;
     const view = event.args.view;
     const viewFrom = view.from;
     const viewTo = view.to;
-    console.log(date, view, viewFrom, viewTo);
-    this.calenderPanel.nativeElement.innerHTML = date.toLocaleDateString();
+    let t = this.eventData.filter((v) =>
+    (new Date(v.StartDate).getFullYear() === date.getFullYear()
+    && new Date(v.StartDate).getDay() === date.getDay()
+    && new Date(v.StartDate).getMonth() === date.getMonth()));
+    const compareFn = (a, b) => {
+      if (a.StartTime < b.StartTime) {
+        return -1;
+      }
+      if (a.StartTime > b.StartTime) {
+        return 1;
+      }
+      return 0;
+    };
+
+    this.selectedDateEvents = t.sort(compareFn);
   }
-  ngAfterViewInit (){
-    this.eventCalender.addSpecialDate(new Date('02/12/2019'), '' , 'Date 1');
-    this.eventCalender.addSpecialDate(new Date('02/13/2019'), '' , 'Date 2');
-    this.eventCalender.addSpecialDate(new Date('02/14/2019'), '' , 'Date 3');
+  ngAfterViewInit () {
+    this.getAllEventsData();
   }
 
   getAllEventsData(): void {
-    if(localStorage.userData){
-    console.log('userData is here' + JSON.stringify(JSON.parse(localStorage.userData)));
-    const uData = JSON.parse(localStorage.userData);
-    this.baseService.loadDashboard(uData.userId, uData.userToken).subscribe((eventRes: any) => {
-        console.log('event resss' + JSON.stringify(eventRes));
-       this.draftCount = eventRes.length;
-    }, (err: HttpErrorResponse) => {
-      alert(err);
+    if (localStorage.userData){
+        console.log('userData is here' + JSON.stringify(JSON.parse(localStorage.userData)));
+        const uData = JSON.parse(localStorage.userData);
+        this.baseService.loadDashboard(uData.userId, uData.userToken).subscribe((eventRes: any) => {
+          this.eventData = eventRes;
+          this.eventDashboardHandler(eventRes);
+        }, (err: HttpErrorResponse) => {
+          alert(err);
+        });
     }
-    );
   }
+  eventDashboardHandler (events: any): void {
+   [this.draftCount, this.pendingCount, this.getDateArray] = events.reduce(([draft, pending, dateArray], v) => {
+         if (v.Status === 'Draft') {
+             draft++;
+         }
+         if (v.Status === 'Pending') {
+             pending++;
+          }
+          if (dateArray.indexOf(v.StartDate)) {
+            dateArray.push(v.StartDate);
+            this.eventCalender.addSpecialDate(new Date(v.StartDate), '' , '');
+          }
+         return [draft, pending, dateArray];
+     }, [0, 0, []]);
   }
-
-
 }
